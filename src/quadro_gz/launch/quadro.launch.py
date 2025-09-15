344cc70152ad271e479eada8736a632fd53a19d9
rahgirrafi <rahgirrafi@gmail.com>
2025-09-15 17:40:39 +0600

    unified ign launch and spawn_enity

--- src/quadro_gz/launch/quadro.launch.py
+++ src/quadro_gz/launch/quadro.launch.py
@@ -4,14 +4,16 @@ from ament_index_python.packages import get_package_share_directory
 from launch.substitutions import PathJoinSubstitution
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
-
-
 def generate_launch_description():
-#launch ignition from ros_ign_gazebo
-    pkg_share = get_package_share_directory('ros_gz_sim')
+    ign_pkg_share = get_package_share_directory('ros_gz_sim')
+    quadro_desc_pkg_share = get_package_share_directory('quadro_description')
 
     gz_sim_launch = PathJoinSubstitution(
-        [pkg_share, 'launch', 'gz_sim.launch.py']
+        [ign_pkg_share, 'launch', 'gz_sim.launch.py']
+    )
+
+    spawn_entity_launch = PathJoinSubstitution(
+        [quadro_desc_pkg_share, 'launch', 'spawn.launch.py']
     )
 
     gazebo = IncludeLaunchDescription(
@@ -19,7 +21,11 @@ def generate_launch_description():
         launch_arguments={'gz_args': 'empty.sdf'}.items(),
     )
 
+    spawn_entity = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(spawn_entity_launch),
+    )
 
     return LaunchDescription([
-        gazebo
+        gazebo,
+        spawn_entity
     ])
